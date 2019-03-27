0bc9f9d2aeb1b013a151d74864582caa168bed1c
camming <cqm@aideacode.com>
2019-03-27 14:01:46 +0800

    Android 7.0权限请求 相册 采集 调用相机等

---
+++ app/src/main/java/portrait/bala/portrait/Cammingctivity.java
@@ -0,0 +1,40 @@
+package portrait.bala.portrait;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import userhead.camming.gz.com.photo.MainActivity;
+
+public class Cammingctivity extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
